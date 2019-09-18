45038d9976dac41689bad345b929b72a306a6fd1
Mshaun_So_Cal <mauriciopichardo@gmail.com>
2019-09-18 14:25:16 -0700

    Updates to Search

--- javascript/searching.js
+++ javascript/searching.js
@@ -1,19 +1,18 @@
+// function changePadding() {
+//   document.parentElement().style.padding = "0px";
+// }
 
-
-
-
-
- $(document).ready(function() {
+$(document).ready(function() {
 $('.searcher').keyup(function() {
-  let searchInput = $('.searcher').val().toLowerCase();
-  console.log(searchInput);
-  $('.featherweight > a').each(function(index, element) {
-    let caption = $(element).attr('data-title').toLowerCase();
-    console.log(caption);
-    if (caption.includes(searchInput)) {
-      $(element).show();
-    } else {
-      $(element).hide();
-    }
-  });
+ let searchInput = $('.searcher').val().toLowerCase();
+ console.log(searchInput);
+ $('.featherweight > a').each(function(index, element) {
+   let info= $(element).attr('data-title').toLowerCase();
+   console.log(info);
+   if (info.includes(searchInput)) {
+     $(element).show();
+   } else {
+     $(element).hide();
+   }
+ });
 }); })
